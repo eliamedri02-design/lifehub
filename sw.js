3b76eb3b2b38beb693ca4ec6319f5aeb953da02c
eliamedri02-design <eliamedri02@gmail.com>
2026-04-20 11:25:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE = 'lifehub-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
+  '/lifehub/',
+  '/lifehub/index.html',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,300;0,400;0,600;1,300;1,400&family=DM+Mono:wght@300;400&display=swap'
 ];
